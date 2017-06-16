eda91877ad837e47bd342898ba31ceb418067936
Hassan K Salim <hassanksalim@gmail.com>
2017-06-16 21:16:17 +0530

    Basic working of Balance Brackets

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -8,17 +8,57 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println(balance("(()(())".toList))
+    
   }
 
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int =
+      if(c == 0 || c == r) 1
+      else pascal(c-1, r-1) + pascal(c, r-1)
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean =
+    {
+
+
+      def moveForward(char: List[Char]): List[Char] =
+      {
+        if (char.isEmpty)
+          List('-')
+        else
+          if (char.head == '(' || char.head == ')') char else moveForward(char.tail)
+      }
+
+      var tempChar = moveForward(chars)
+
+      def checkBalance(): Boolean =
+      {
+
+        var isBalanced = true
+        if(tempChar.head == '(') { isBalanced = false; tempChar = moveForward(tempChar.tail) }
+        if(tempChar.head == '(' && !isBalanced) checkBalance()
+        if(tempChar.head == ')' && isBalanced) return false
+        if (tempChar.head == ')' && !isBalanced)
+        {
+          isBalanced = true
+          tempChar = moveForward(tempChar.tail)
+        }
+        if(tempChar.head == '-')
+          isBalanced
+        else
+        {
+          tempChar = moveForward(tempChar)
+          checkBalance()
+        }
+      }
+      checkBalance()
+    }
   
   /**
    * Exercise 3
